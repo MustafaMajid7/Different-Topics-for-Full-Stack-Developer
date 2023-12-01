e8bae205de11a1234aef0b7d14aa7f3c58252058
mostafa maged <eng.mustafamaged98@gmail.com>
2023-12-01 23:52:01 +0200

    experienceing with record and yield keywords in c#

---
+++ C_Sharp/C_Sharp_Record/Program.cs
@@ -0,0 +1,170 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace C_Sharp_Record
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			// class without any override functions
+
+			Point point1 = new Point(10, 5);
+			Point point2 = new Point(10, 5);
+			Console.WriteLine(point1 == point2);
+			Console.WriteLine(point1.Equals(point2));
+
+			// class with override Equals object function 
+
+			Point_Equals point3 = new Point_Equals(10, 5);
+			Point_Equals point4 = new Point_Equals(10, 5);
+			Console.WriteLine(point3 == point4);
+			Console.WriteLine(point3.Equals(point4));
+			Console.WriteLine(point3.GetHashCode());
+			Console.WriteLine(point4.GetHashCode());
+
+			// class with override == operator
+
+			Point_equal_operator point5 = new Point_equal_operator(10, 5);
+			Point_equal_operator point6 = new Point_equal_operator(10, 6);
+			Console.WriteLine(point5 != point6);
+			Console.WriteLine(point5.Equals(point6));
+
+            // class with override == operator
+
+            Console.WriteLine("********************************************");
+
+            PointEquatable point7 = new PointEquatable(10, 5);
+			PointEquatable point8 = new PointEquatable(10, 5);
+			Console.WriteLine(point7.Equals(point8));
+
+			Console.WriteLine("********************************************");
+
+			SPoint point9 = new SPoint(10, 5);
+			SPoint point10 = new SPoint(10, 5);
+			Console.WriteLine(point9.Equals(point10));
+
+			Console.WriteLine("********************************************");
+
+			RPoint rPoint1 = new RPoint
+			{
+				x = 10,
+				y = 10,
+			};
+			rPoint1.y = 15;
+			RPoint rPoint2 = new RPoint(10, 5);
+			RPoint rPoint3 = rPoint2 with {x=30};
+
+			Console.WriteLine(rPoint1);
+            Console.WriteLine(rPoint2);
+            Console.WriteLine(rPoint3);
+
+		}
+	}
+	class Point
+	{
+        public int X { get; set; }
+		public int Y { get; set; }
+        public Point(int X,int Y)
+        {
+            this.X = X;
+			this.Y = Y;
+        }
+		
+	}
+	class Point_Equals
+	{
+		public int X { get; set; }
+		public int Y { get; set; }
+		public Point_Equals(int X, int Y)
+		{
+			this.X = X;
+			this.Y = Y;
+		}
+		public override bool Equals(object? obj)
+		{
+			Point_Equals point = obj as Point_Equals;
+			if (point == null)
+			{
+				return false;
+			}
+			return this.X == point.X && this.Y == point.Y;
+		}
+		public override int GetHashCode()
+		{
+			//int hash;
+			//hash = 17*11 +X.GetHashCode();
+			//hash += 17*11 +Y;
+			//return hash;
+			return HashCode.Combine(X,Y);
+		}
+	}
+	class Point_equal_operator
+	{
+		public int X { get; set; }
+		public int Y { get; set; }
+		public Point_equal_operator(int X, int Y)
+		{
+			this.X = X;
+			this.Y = Y;
+		}
+		public static bool operator ==(Point_equal_operator lhs,Point_equal_operator rhs)
+		{
+            if (lhs is null)
+            {
+				if (rhs == null)
+					return true;
+
+				return false;
+            }
+			return lhs.X == rhs.X && lhs.Y == rhs.Y;
+        }
+		public static bool operator !=(Point_equal_operator lhs, Point_equal_operator rhs)
+		{
+			return !(lhs == rhs);
+		}
+	}
+	struct PointEquatable : IEquatable<PointEquatable> 
+	{
+		public int X { get; set; }
+		public int Y { get; set; }
+		public PointEquatable(int X, int Y)
+		{
+			this.X = X;
+			this.Y = Y;
+		}
+		public override bool Equals(object? obj)
+		{
+			if (obj is null)
+			{
+				return false;
+			}
+			return this.Equals(obj);
+		}
+		public bool Equals(PointEquatable other)
+		{
+			return other.X == this.X && other.Y == this.Y;
+		}
+	}
+	struct SPoint
+	{
+		public int X { get; set; }
+		public int Y { get; set; }
+		public SPoint(int X, int Y)
+		{
+			this.X = X;
+			this.Y = Y;
+		}
+		public override bool Equals(object? obj)
+		{
+			SPoint? sPoint = obj as SPoint?;
+			//SPoint sPoint = (SPoint)obj;
+			return this.X == sPoint?.X && this.Y == sPoint?.Y;
+		}
+	}
+	record struct RPoint (int x, int y)
+	{
+  //      public int X { get; }
+		//public int Y { get; }
+		public RPoint():this(0,0) { }
+	};
+}
---
+++ C_Sharp/YieldReturn/Program.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+
+namespace YieldReturn
+{
+	internal class Program
+	{
+		private static List<int> myList = new List<int>();
+		private static object lockObject = new object();
+
+		static List<int> list = Enumerable.Range(1, 5).ToList();
+		static void Main(string[] args)
+		{
+
+			//Thread writerThread = new Thread(WriterThreadMethod);
+			//Thread readerThread = new Thread(ReaderThreadMethod);
+
+			//writerThread.Start();
+			//readerThread.Start();
+
+			//writerThread.Join();
+			//readerThread.Join();
+
+			//-----------------------------------------------------------------------------
+
+			var t1 = new Thread(print);
+			var t2 = new Thread(() =>
+			{
+                while (true)
+                {
+					lock (lockObject) { 
+						Thread.Sleep(500);
+						list.Add(list.Max() + 1);
+					}
+				}
+            });
+			t1.Start();
+			t2.Start();
+			t1.Join();
+			t2.Join();
+			//Console.Read();
+
+		}
+		public static IEnumerable<int> yieldReturn()
+		{
+            foreach (var item in list)
+            {
+				lock(lockObject)
+				{
+					yield return item;
+				}
+            }
+			
+		}
+		public static void print()
+		{
+			Console.WriteLine(yieldReturn());
+			//        foreach (var item in list)
+			//        {
+			//lock( lockObject)
+			//{
+			//	Thread.Sleep(500);
+			//	Console.WriteLine(item);
+			//}
+			//        }
+		}
+
+		static void WriterThreadMethod()
+		{
+			while(true) { 
+				for (int i = 0; i < 10; i++)
+				{
+					lock (lockObject)
+					{
+						myList.Add(i);
+						Console.WriteLine($"Writer Thread added: {i}");
+						Thread.Sleep(100); // Simulate some work
+					}
+				}
+			}
+		}
+
+		static void ReaderThreadMethod()
+		{
+			while (true)
+			{
+				lock (lockObject)
+				{
+					Console.WriteLine("Reader Thread content:");
+					foreach (var item in myList)
+					{
+						Console.Write($"{item} ");
+					}
+					Console.WriteLine();
+					Thread.Sleep(1000); // Print every second
+				}
+			}
+		}
+	}
+
+}
